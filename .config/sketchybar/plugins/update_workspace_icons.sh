6b2a7083409afc30c1e29bf1baaf1b1efdb77801
Jack Michell <jmichell2003@gmail.com>
2025-09-22 08:18:21 -0500

    aerospace and sketchybar config changes

--- .config/sketchybar/plugins/update_workspace_icons.sh
+++ .config/sketchybar/plugins/update_workspace_icons.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-
 CONFIG_DIR="$HOME/.config/sketchybar"
 
 update_space_icons() {
     local sid=$1
+    local focused_workspace=$(aerospace list-workspaces --focused)
     local apps=$(aerospace list-windows --workspace "$sid" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
-
-    sketchybar --set space.$sid drawing=on
-
-    if [ "${apps}" != "" ]; then
-        icon_strip=" "
-        while read -r app; do
-            icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-        done <<<"${apps}"
+    
+    # Check if workspace has apps or is currently focused
+    if [ -n "$apps" ] || [ "$sid" = "$focused_workspace" ]; then
+        # Show workspace if it has apps or is focused
+        sketchybar --set space.$sid drawing=on
+        
+        if [ -n "$apps" ]; then
+            icon_strip=" "
+            while read -r app; do
+                icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+            done <<<"$apps"
+        else
+            icon_strip=""
+        fi
+        sketchybar --set space.$sid label="$icon_strip"
     else
-        icon_strip=""
+        # Hide empty workspace
+        sketchybar --set space.$sid drawing=off
     fi
-    sketchybar --set space.$sid label="$icon_strip"
 }
 
 # Update all workspaces to ensure clean state
